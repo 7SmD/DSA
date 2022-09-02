550ab1ffbcc3873e63bab44c95a42617d18cee51
Sudhang <80188078+7SmD@users.noreply.github.com>
2022-09-02 15:57:36 +0530

    Add files via upload

---
+++ INTS/MaxPathSum_Leaf2Leaf.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution
+{
+public:
+    int f(TreeNode *root, int &res)
+    {
+        if (!root)
+        {
+            return 0;
+        }
+        int l = f(root->left, res);
+        int r = f(root->right, res);
+
+        int temp = root->val + max(l, r);
+        int ans = max(temp, root->val + l + r);
+        res = max(res, ans);
+
+        return temp;
+    }
+
+    int maxPathSum(TreeNode *root)
+    {
+        int res = INT_MIN;
+        f(root, res);
+        return res;
+    }
+};
